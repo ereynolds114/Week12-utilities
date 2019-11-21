4ed0b82860a7fa51e78f406b31789f3077ed2865
Evan Reynolds <ereynolds@mymail.mines.edu>
2019-11-21 08:47:12 -0700

    finished first 4 functions

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,14 +1,36 @@
-
+#ereynolds114/Week12-utilities
+#Evan Reynolds
+#CSCI 102-A
+#Week 11-B
 
 
 
 def PrintOutput(out):
+    print('OUTPUT ',str(out))
 
 def LoadFile(name):
+    file=open(name,'r')
+    contents=file.read()
+    contents=contents.split('''
+''')
+    file.close()
+    return contents
 
 def UpdateString(string,replace,index):
+    newstr=''
+    for i in range(0,len(string)):
+        if i==index:
+            newstr+=replace
+        else:
+            newstr+=string[i]
+    return newstr
 
 def FindWordCount(large,small):
+    count=0
+    for i in large:
+        if i==small:
+            count+=1
+    return count
 
 def ScoreFinder(players,scores,who):
 
