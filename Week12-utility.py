4eb7fd92df38c2957bc843c1ace8d0f5b40ba41c
BuildTools <unconfigured@null.spigotmc.org>
2019-11-21 18:11:29 -0700

    finished functions

--- Week12-utility.py
+++ Week12-utility.py
@@ -23,7 +23,7 @@ def UpdateString(string,replace,index):
             newstr+=replace
         else:
             newstr+=string[i]
-    return newstr
+    PrintOutput(newstr)
 
 def FindWordCount(large,small):
     count=0
@@ -33,10 +33,29 @@ def FindWordCount(large,small):
     return count
 
 def ScoreFinder(players,scores,who):
+    a=-1
+    who=who.lower()
+    for i in range(0,len(players)):
+        check=players[i].lower()
+        if check==who:
+            a=i
+    if a==-1:
+        PrintOutput('player not found')
+    else:
+        PrintOutput(who+' got a score of '+str(scores[a]))
 
 def Union(first,second):
+    first = list(dict.fromkeys(first))
+    second=list(dict.fromkeys(second))
+    return first+second
 
 def Intersection(first,second):
+    return list(set(first) & set(second))
+    
 
 def NotIn(first,second):
+    return list(set(first)-set(second))
 
+players = ["Mary", "Cody", "Joe", "Jill", "Xai", "Bodo"]
+scores = [5, 8, 10, 6, 10, 4]
+players2 = ["Melvin", "Martian", "Baka", "Xai", "Cody"]
